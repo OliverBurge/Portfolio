8f7b033d767142cbcbd0e8efec98a0d470a5309f
Ollie B <oliver.burge2014@gmail.com>
2018-07-26 10:46:37 +0100

    Implemented concern for image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :percet_utilized
+
+	after_initialize :set_defaults
+
+	def set_defaults 
+		self.badge ||= Placeholder.generator(height: '250', width: '250')
 end
