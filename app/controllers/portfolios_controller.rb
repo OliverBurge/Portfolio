e73496e8b4e7ef05b55de89fc5a8a404832034c1
Ollie B <oliver.burge2014@gmail.com>
2018-08-22 15:27:19 +0100

    Integrated new gem into app for copyright

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -48,12 +48,12 @@ class PortfoliosController < ApplicationController
 
   def destroy
     # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_items = Portfolio.find(params[:id])
     # Destroy/Delete the record
-    @portfolio_item.destroy
+    @portfolio_items.destroy
     # Redirect
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Portfolio item was successfully deleted.' }
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully deleted.' }
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,7 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  def copyright_generator
+    OjbViewTool::Renderer.copyright 'Oliver Burge', 'All rights reserved'
+  end
 end
